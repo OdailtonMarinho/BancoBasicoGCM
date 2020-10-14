f29a8eb016bb0888740d6a804b1a11fc74b79da8
Odailton Marinho <odailton_marinho1000@hotmail.com>
2020-10-14 15:43:25 -0300

    Adicionada funcionalidade de obter conta do DataBase e melhorada a forma de armazenamento

--- DataBase/SaveData.php
+++ DataBase/SaveData.php
@@ -3,10 +3,27 @@
 	{
 		public static function write_conta($nome, $numero_conta, $agencia)
 		{
-			$str_data = '{nome:'.$nome.',numero_conta:'.$numero_conta.',agencia:'.$agencia.'}';
+			$data = array('nome'=>$nome, 'numero_conta'=>$numero_conta, 'agencia'=>$agencia);
 
-			$file = fopen('DataBase.json', 'w');
-			fwrite($file, $str_data);
+			$str_data = json_encode($data);
+			$file = fopen('DataBase.json', 'a');
+			fwrite($file, $str_data.PHP_EOL);
+
+			fclose($file);
+		}
+
+		public static function get_conta($numero_conta)
+		{
+			$arr_data = file('DataBase.json');
+
+			foreach($arr_data as $conta)
+			{
+				$json_conta = json_decode($conta);
+				var_dump($json_conta->numero_conta);
+				if($json_conta->numero_conta == $numero_conta) return $json_conta;
+			}
+
+			return false;
 		}
 	}
 ?>
