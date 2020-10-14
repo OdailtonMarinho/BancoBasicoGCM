b9d72f9d2162a7f1eef6eaf479f8f9d53dd62612
Odailton Marinho <odailton_marinho1000@hotmail.com>
2020-10-14 13:26:26 -0300

    Adicionado Classe de controle dos Commands

---
+++ Model/BancoController.php
@@ -0,0 +1,27 @@
+<?php
+	include 'ICommand.php';
+	include 'Transferencia.php';
+	include 'Debito.php';
+	include 'Saldo.php';
+
+	class BancoController
+	{
+		public $comandos;
+		public $comandos_size;
+		public function __construct()
+		{
+			$comandos = array();
+			$comandos_size = 0;
+		}
+
+		public executar_comandos()
+		{
+			for ($i = 0; i < $comandos_size; i++)
+			{
+				$comando[$i]->execute();
+				unset($comando);
+				$comandos_size = 0;
+			}
+		}
+	}
+?>
